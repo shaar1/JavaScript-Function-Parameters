7913a0798736f630ef1982986045146f8b6d4d60
Abdallah <abdallahchaar90@gmail.comh>
2021-01-07 12:58:45 +0100

    träning om olika parmeters

---
+++ main.js
@@ -0,0 +1,89 @@
+/*
+function myAgeInDays() {
+
+"use strict";
+
+var myAge = 30;
+
+return 30 * 365;
+
+}
+
+var daysClac = myAgeInDays();
+
+document.getElementById("test").innerHTML = 
+"Your Age In Days = " + daysClac;
+*/
+
+
+/*
+function myAgeInDays(myAge) {
+
+    "use strict";
+    
+    
+    return myAge * 365;
+    
+    }
+    
+    var daysClac = myAgeInDays();
+    
+    document.getElementById("test").innerHTML = 
+
+    "Your Age In Days = " + daysClac;
+    */
+/*
+
+   function myAgeInDays(myAge) {
+
+    "use strict";
+    
+    
+    return myAge * 365;
+    
+    }
+    
+    var daysClac = myAgeInDays(30);
+    
+    document.getElementById("test").innerHTML = 
+
+    "Your Age In Days = " + daysClac;
+    */
+
+/*
+   function myAgeInDays(myAge) {
+
+    "use strict";
+    
+    
+    return myAge * 365;
+    
+    }
+    
+    document.getElementById("test").innerHTML = "Your Age In Days = " + myAgeInDays(30);
+    */
+
+/*
+   function myAgeInDays(myAge) {
+
+    "use strict";
+    
+    
+    return myAge * 365;
+    
+    }
+    
+    document.getElementById("test").innerHTML = "Your Age In Days = " + myAgeInDays("Abdallah");
+*/
+
+
+function sayHello(theName) {
+
+    "use strict";
+
+
+    return "Hello " + theName + " How Are You Today"
+
+}
+
+document.getElementById("test").innerHTML = sayHello("Abdallah");
